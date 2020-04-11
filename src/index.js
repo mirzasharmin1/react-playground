6c4159386561fcde9abc1a7dcff5353b7907a6ad
Mirza Sharmin <mirza.sharmin1@gmail.com>
2020-04-11 11:10:52 -0400

    I added a console log

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.log("Hello this is my log");
 ReactDOM.render(
   <React.StrictMode>
     <App />
